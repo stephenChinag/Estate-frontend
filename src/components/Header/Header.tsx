da5f6d76bdb0f21574a6582f9a5490e35bdcab18
StpehenChinag <stephenchinag@gmail.com>
2024-04-20 23:37:19 +0100

    mob

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,20 @@
+import { Link } from "react-router-dom";
+import MobileNav from "../Mobile/MobileNav";
+
 export default function Header() {
-  return <div>header</div>;
+  return (
+    <div className="border-b-2 border-b-orange-500 py-6">
+      <div className="container mx-auto flex justify-between items-center ">
+        <Link
+          to="/"
+          className="text-3xl font-bold tracking-tight text-orange-500"
+        >
+          Labisi Food
+        </Link>
+        <div className="md:hidden">
+          <MobileNav />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Layout/RootLayout.tsx
+++ src/components/Layout/RootLayout.tsx
@@ -5,7 +5,10 @@ export default function RootLayout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <Outlet />
+
+      <div className="container mx-auto flex-1 py-10">
+        <Outlet />
+      </div>
     </div>
   );
 }
