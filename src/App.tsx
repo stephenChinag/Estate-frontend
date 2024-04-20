1b4a86f48c81b81fbb1134f9e333c743240718ea
StpehenChinag <stephenchinag@gmail.com>
2024-04-20 16:25:57 +0100

    root

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import "./App.css";
 //import { Button } from "./components/ui/button";
-import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import User from "./pages/User";
+import RootLayout from "./components/Layout/RootLayout";
 
 function App() {
-  createBrowserRouter([{ path: "/", element: <Home /> }]);
-  return <div>Hello</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "user", element: <User /> },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Error.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Error() {
+  return <div>Could not find this page</div>;
+}
