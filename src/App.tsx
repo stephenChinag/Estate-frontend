e8128d94595fe853bc94f5816d96dcbd6429cdc0
StpehenChinag <stephenchinag@gmail.com>
2024-05-02 18:40:37 +0100

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,10 @@
 // import "./App.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-import Login from "./pages/Login/Login";
-import SignUp from "./pages/Signup/Signup";
-import Documents from "./pages/Document/Documents";
-import Shared from "./pages/Shared/Shared";
-import Setting from "./pages/Setting/Setting";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import Siderbar from "./components/Sidebar/Siderbar";
 
 export default function App() {
-  const route = createBrowserRouter([
-    { path: "/login", element: <Login /> },
-    { path: "/Signup", element: <SignUp /> },
-    {
-      path: "/",
-      element: <Siderbar />,
-
-      children: [
-        {
-          path: "dashboard",
-          element: <Dashboard />,
-        },
-        {
-          path: "document",
-          element: <Documents />,
-        },
-        {
-          path: "shared",
-          element: <Shared />,
-        },
-        {
-          path: "setting",
-          element: <Setting />,
-        },
-      ],
-    },
-  ]);
-  return <RouterProvider router={route} />;
+  return (
+    <div className="layout">
+      {" "}
+      <h1> Lay Out</h1>
+    </div>
+  );
 }
