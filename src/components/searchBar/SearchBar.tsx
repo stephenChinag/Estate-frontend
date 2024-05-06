7d4a5e47823a15fa3eb779f1f98f12687e54a676
StpehenChinag <stephenchinag@gmail.com>
2024-05-06 19:30:10 +0100

    serarch

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import "./searchBar.scss";
 
 export default function SearchBar() {
+  const [] = useState({
+    type: "buy",
+    location: "",
+  });
   return (
-    <>
-      <p>Search</p>
-    </>
+    <div className="searchBar">
+      <div className="type">
+        <button>Buy</button> <button>Rent</button>
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="city Location" />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={10000000}
+          placeholder="Max Price"
+        />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={10000000}
+          placeholder="Min Price"
+        />
+      </form>
+    </div>
   );
 }
