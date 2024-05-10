4d90f2b7b8781be59f74019b00762c0a80e73436
StpehenChinag <stephenchinag@gmail.com>
2024-05-10 19:42:27 +0100

    save

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
+
 import location from "../../assets/location.png";
+import bath from "../../assets/bath.png";
+import bed from "../../assets/bed.png";
 import "./card.scss";
 interface Item {
   id: number;
@@ -24,14 +27,27 @@ export default function Card({ item }: CardProps) {
         <img src={item.img} />
       </Link>
       <div className="textContainer">
-        <h2>
+        <h2 className="title">
           <Link to={`/${item.id}`}>{item.title}</Link>
         </h2>
         <p className="address">
           <img src={location} alt="location" />
           <span> {item.address}</span>
         </p>
-        <p className="price"> {item.price} </p>
+        <p className="price">Naira {item.price} </p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src={bed} alt="" />
+              <span> {item.bedroom} bedroom </span>
+            </div>
+            <div className="feature">
+              <img src={bath} alt="" />
+              <span> {item.bathroom} bathroom </span>
+            </div>
+          </div>
+          <div className="icons"></div>
+        </div>
       </div>
     </div>
   );
